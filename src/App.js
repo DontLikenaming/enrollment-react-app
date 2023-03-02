93f7dac1627706dd722dd411655b538545c97c72
DontLikenaming <edirna21@gmail.com>
2023-03-02 16:45:51 +0900

    대학생 등록 폼 작성

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EnrollmentForm from './components/EnrollmentForm';
+
+const App = () => {
+    return(
+        <div>
+            <EnrollmentForm />
+        </div>
+    );
+};
+
+export default App;
